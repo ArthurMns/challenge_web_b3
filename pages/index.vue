277d0895374decb2c2f307ec0b820c3013495eaa
Andoni Lalanne-Berdouticq <lalanne.andoni1@gmail.com>
2024-10-22 11:53:14 +0200

    Error resolved

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,48 @@
 <template>
   <div class="carousel w-full h-96">
-    <div id="slide2" class="carousel-item relative w-full">
-      <img src="/assets/photos/doggy.png" class="w-full" alt="" />
+    <!-- Slide 1 -->
+    <div id="slide1" class="carousel-item relative w-full">
+      <img src="/assets/photos/animalerie.png" class="w-full" alt="Slide 1" />
       <div
         class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between"
       >
-        <a href="#slide3" class="btn btn-circle">❮</a>
+        <a href="#slide4" class="btn btn-circle">❮</a>
+        <a href="#slide2" class="btn btn-circle">❯</a>
+      </div>
+    </div>
+
+    <!-- Slide 2 -->
+    <div id="slide2" class="carousel-item relative w-full">
+      <img src="/assets/photos/doggy.png" class="w-full" alt="Slide 2" />
+      <div
+        class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between"
+      >
+        <a href="#slide1" class="btn btn-circle">❮</a>
         <a href="#slide3" class="btn btn-circle">❯</a>
       </div>
     </div>
+
+    <!-- Slide 3 -->
     <div id="slide3" class="carousel-item relative w-full">
       <img
         src="/assets/photos/pet-store.png"
         class="w-full h-64 object-cover"
-        alt=""
+        alt="Slide 3"
       />
       <div
-        class="absolute left-4 right-4 top-1/2 flex -translate-y-1/2 transform justify-between"
+        class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between"
       >
         <a href="#slide2" class="btn btn-circle">❮</a>
         <a href="#slide4" class="btn btn-circle">❯</a>
       </div>
     </div>
+
+    <!-- Slide 4 -->
     <div id="slide4" class="carousel-item relative w-full">
       <img
         src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
         class="w-full h-64 object-cover"
-        alt=""
+        alt="Slide 4"
       />
       <div
         class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between"
@@ -37,4 +53,5 @@
     </div>
   </div>
 </template>
+
 <script setup lang="ts"></script>
